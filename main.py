b591122e55977175aeade568619492008d923fd2
Ludvig-Rav <82057425+Ludvig-Rav@users.noreply.github.com>
2021-09-16 13:03:34 +0200

    Change to faster choice of user input.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import math
-print("Hello and welcome, please enter if you need calculation for a cube or a tetrahedron")
-shape = input("cube or tetrahedron\n")
+print("Hello and welcome")
+choice = "yes"
+while choice == "yes":
+    print("please enter if you need calculation for a cube or a tetrahedron")
+    shape = input("c for cube or t for tetrahedron\n")
 
-if shape == "cube":
-    print("Enter cm of one side of the cube")
-    a = int(input())
-    v = a**3
-    print("The volume of the cube is: "); print(v)
+    if shape == "c":
+        print("Enter cm of one side of the cube")
+        a = int(input())
+        v = a ** 3
+        print("The volume of the cube is: ");
+        print(v)
+        print("Do you want to go again? yes or no. ")
+        choice = input("")
 
-if shape == "tetrahedron":
-    print("enter cm of the sides of the tetrahedron")
-    a = int(input())
-    v = (a**3/(6 * math.sqrt(2)))
-    print("The volume of the tetrahedron is: "); print(v)
+    if shape == "t":
+        print("enter cm of the sides of the tetrahedron")
+        a = int(input())
+        v = (a ** 3 / (6 * math.sqrt(2)))
+        print("The volume of the tetrahedron is: ");
+        print(v)
+        print("Do you want to go again? yes or no. ")
+        choice = input("")
