d0ed742cd4ba6f24d1346e61ea0bc566477d8c2a
Ludvig Ravelin <ludvig.skinstad@gmail.com>
2021-09-20 12:42:02 +0200

    added functions

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import math
 print("Hello and welcome")
 choice = "yes"
+
+def cube_vol (a):
+    v = a ** 3
+    return v
+
+def tri_vol (a):
+    v = (a ** 3 / (6 * math.sqrt(2)))
+    return v
+
 while choice == "yes":
     print("please enter if you need calculation for a cube or a tetrahedron")
     shape = input("c for cube or t for tetrahedron\n")
@@ -8,17 +17,15 @@ while choice == "yes":
     if shape == "c":
         print("Enter cm of one side of the cube")
         a = int(input())
-        v = a ** 3
-        print("The volume of the cube is: ");
-        print(v)
+        volume = cube_vol(a)
+        print(f"The volume of the cube is: {volume} ");
         print("Do you want to go again? yes or no. ")
         choice = input("")
 
     if shape == "t":
         print("enter cm of the sides of the tetrahedron")
         a = int(input())
-        v = (a ** 3 / (6 * math.sqrt(2)))
-        print("The volume of the tetrahedron is: ");
-        print(v)
+        volume = tri_vol(a)
+        print(f"The volume of the tetrahedron is: {volume} ")
         print("Do you want to go again? yes or no. ")
         choice = input("")
