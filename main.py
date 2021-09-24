e27122fc43e57ce0e12b5730f2825cfb35e408ce
Ludvig Ravelin <ludvig.skinstad@gmail.com>
2021-09-24 10:44:51 +0200

    Added menu, error Handling and some if statement changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-print("Hello and welcome")
-choice = "yes"
+#choice = "yes"
 result = []
 
 def cube_vol (a):
@@ -15,44 +14,56 @@ def oct_vol (a):
     v = (2*(1+(math.sqrt(2)))*a*a)
     return v
 
-while choice == "yes":
-    print("please enter if you need calculation for a cube, a tetrahedron or a octagon")
-    shape = input("c for cube, t for tetrahedron and o for octagon\n")
+def show_menu():
+    print("\n===========================================")
+    print("|                  Welcome                |")
+    print("|         To the world of geometry        |")
+    print("|         1. Calculate V of a cube        |")
+    print("|         2. Calculate V of a tetrahedron |")
+    print("|         3. Calculate V of a octagon     |")
+    print("|         4. Exit                         |")
+    print("===========================================")
 
-    if shape == "c":
-        print("Enter cm of one side of the cube")
-        a = int(input())
+
+while True:
+    show_menu()
+    try:
+        menu_choice = int(input("Choose 1, 2, 3 or 4 to exit "))
+    except ValueError:
+        print("--->You cannot choose a float digit<---")
+        menu_choice = 0
+       
+    if menu_choice == 1:
+        a = int(input("Enter cm of one side of the cube "))
         volume = cube_vol(a)
         result.append("C")
         result.append(volume)
         print(f"The volume of the cube is: {volume} ");
-        print("Do you want to go again? yes or no. ")
-        choice = input("")
 
-    if shape == "t":
-        print("enter cm of the sides of the tetrahedron")
-        a = int(input())
+    elif menu_choice == 2:
+        a = int(input("enter cm of the sides of the tetrahedron "))
         volume = tri_vol(a)
         result.append("T")
         result.append(volume)
         print(f"The volume of the tetrahedron is: {volume} ")
-        print("Do you want to go again? yes or no. ")
-        choice = input("")
 
-    if shape == "o":
-        print("enter cm of the sides of the octagon")
-        a = int(input())
+    elif menu_choice == 3:
+        a = int(input("enter cm of the sides of the octagon "))
         volume = oct_vol(a)
         result.append("O")
         result.append(volume)
         print(f"The volume of the octagon is: {volume} ")
-        print("Do you want to go again? yes or no. ")
-        choice = input("")
 
-    seeResult = input("Do you want to see the result? (y/n) ")
-    if seeResult == "y":
-        print(f"This is the results so far {result}")
-
-if choice == "no":
-    print("Hasta la vista!")
-    exit()
+    elif menu_choice == 4:
+        seeResult = input("Do you want to see the result? (y/n) ")
+        if seeResult == "y":
+            print(f"This is the volumes you have calculated: {result}")
+            print("Hasta la vista! Thanks for using my program!")
+            exit()
+        elif seeResult == "n":
+            print("Hasta la vista! Thanks for using my program!")
+            exit()
+        else:
+            print("--->Incorrect choice, have another go<---")
+    else:
+        print("-->Incorrect choice, please try again<--")
